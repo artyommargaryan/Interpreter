94d320e816be5137de4b6939626bbfd478b9e61a
Artyom Margaryan <89930588+artyommargaryan@users.noreply.github.com>
2022-10-20 01:10:49 +0400

    Update Interp3.java

--- Interp3.java
+++ Interp3.java
@@ -290,10 +290,6 @@ public class Interp3 {
                 return num1 * num2;
             case "/":
                 return num1 / num2;
-            case "%":
-                return num1 % num2;
-            case "^":
-                return (int) Math.pow(num1, num2);
             default:
                 throw new RuntimeException("Syntax Error!!!\nUnknown Operator: " + lineElem[1]);
         }
